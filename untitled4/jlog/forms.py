4a574fa4f952b3ecbfc8f5036a2404823dd992a3
JL <jei888@qq.com>
2016-06-06 22:38:02 +0800

    elease enter the commit message for your changes. Lines starting 	deleted:    untitled4/.idea/misc.xml 	deleted:    untitled4/.idea/modules.xml 	deleted:    untitled4/.idea/untitled4.iml 	deleted:    untitled4/.idea/workspace.xml 	deleted:    untitled4/jlog/__init__.py 	deleted:    untitled4/jlog/__pycache__/__init__.cpython-35.pyc 	deleted:    untitled4/jlog/__pycache__/admin.cpython-35.pyc 	deleted:    untitled4/jlog/__pycache__/models.cpython-35.pyc 	deleted:    untitled4/jlog/__pycache__/views.cpython-35.pyc 	deleted:    untitled4/jlog/admin.py 	deleted:    untitled4/jlog/apps.py 	deleted:    untitled4/jlog/forms.py 	deleted:    untitled4/jlog/migrations/__init__.py 	deleted:    untitled4/jlog/migrations/__pycache__/__init__.cpython-35.pyc 	deleted:    untitled4/jlog/models.py 	deleted:    untitled4/jlog/static/css/bootstrap.css 	deleted:    untitled4/jlog/static/css/bootstrap.min.css 	deleted:    untitled4/jlog/static/css/carousel.css 	deleted:    untitled4/jlog/static/css/navbar-fixed-top.css 	deleted:    untitled4/jlog/static/js/bootstrap.js 	deleted:    untitled4/jlog/static/js/bootstrap.min.js 	deleted:    untitled4/jlog/static/js/jquery.min.js 	deleted:    untitled4/jlog/tests.py 	deleted:    untitled4/jlog/views.py 	deleted:    untitled4/manage.py 	deleted:    untitled4/templates/_base_.html 	deleted:    untitled4/templates/create.html 	deleted:    untitled4/templates/index.html 	deleted:    untitled4/untitled4/__init__.py 	deleted:    untitled4/untitled4/__pycache__/__init__.cpython-35.pyc 	deleted:    untitled4/untitled4/__pycache__/settings.cpython-35.pyc 	deleted:    untitled4/untitled4/__pycache__/urls.cpython-35.pyc 	deleted:    untitled4/untitled4/__pycache__/wsgi.cpython-35.pyc 	deleted:    untitled4/untitled4/settings.py 	deleted:    untitled4/untitled4/urls.py 	deleted:    untitled4/untitled4/wsgi.py

--- untitled4/jlog/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-class BlogForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    category = forms.CharField(max_length=15)
-    content = forms.TextField()
-    timestamp = forms.DateTimeField()
